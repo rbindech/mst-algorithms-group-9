e3ac21583f4f0bbaa4412df4b4f28179d338a95d
Raoul-afk <calixteberthier@gmail.com>
2026-09-15 16:43:45 +0700

    Update prim_algorithm.py

--- prim_algorithm.py
+++ prim_algorithm.py
@@ -1,1 +1,16 @@
 from graph_network import edges, nodes
+starting_node = edges[0][0]
+current_edges = []
+neighbor_edges = []
+
+def Next_Nodes(current_edges)
+    for edge in edges:
+        node1, node2, weight = edge
+        if node1 in current_edges and node2 not in visited:
+            neighbor_edges.append (node2)
+  
+        if node2 == current_node and node1 not in visited:
+            neighbor_edges.append(node1)
+          
+def IsthereNeighboor(current_edges)
+ for e               
